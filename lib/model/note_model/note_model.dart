57796c59a8673f7d1e1cea124251be02f6be2db2
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-05-07 22:51:07 +0500

    note model is added

---
+++ lib/model/note_model/note_model.dart
@@ -0,0 +1,29 @@
+
+
+class NoteModel {
+  String? id;
+  String? userId;
+  String? title;
+  String? content;
+  String? date;
+
+  NoteModel({this.id, this.userId, this.title, this.content, this.date});
+
+  NoteModel.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    userId = json['userId'];
+    title = json['title'];
+    content = json['content'];
+    date = DateTime.tryParse(json['date']) as String?;
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
+    data['userId'] = this.userId;
+    data['title'] = this.title;
+    data['content'] = this.content;
+    data['date'] = this.date;
+    return data;
+  }
+}
