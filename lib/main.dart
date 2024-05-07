45b62e09f42cfdbda0b2de35d30785cb077db26d
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-05-07 22:53:09 +0500

    updated for bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app_with_node/view/home_view/home_view.dart';
+import 'package:notes_app_with_node/view_model/bloc/notes_bloc/note_bloc.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,15 +14,21 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: Scaffold(
-        body: Column(),
-      ),
+    return MultiBlocProvider(
+        providers: [
+          BlocProvider(create: (_) => NotesBloc()),
+        ],
+        child: MaterialApp(
+          title: 'Flutter Demo',
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+            useMaterial3: true,
+          ),
+          home: Scaffold(
+            body: HomeView(),
+          ),
+        )
     );
   }
 }
